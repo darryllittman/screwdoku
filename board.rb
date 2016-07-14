4eabcb67a6b66b683c63353de94a68651a038a59
App Academy Student <student@appacademy.io>
2016-07-14 12:05:08 -0700

    fixed from_file method in board to preprocess empty rows in file

--- board.rb
+++ board.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative "tile"
 
 class Board
@@ -9,11 +10,11 @@ class Board
 
   def self.from_file(filename)
     rows = File.readlines(filename).map(&:chomp)
+    rows.delete_if { |row| row.empty? }
     tiles = rows.map do |row|
       nums = row.split("").map { |char| Integer(char) }
       nums.map { |num| Tile.new(num) }
     end
-
     self.new(tiles)
   end
 
